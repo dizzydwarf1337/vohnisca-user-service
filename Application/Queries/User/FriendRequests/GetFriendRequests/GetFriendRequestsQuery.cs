bd5ae11e6ffea9b3acc6cfc70cf4a2cbcc9f0490
dizzydwarf1337 <stormspirit19735@gmail.com>
2026-05-17 11:02:51 +0200

    test

--- Application/Queries/User/FriendRequests/GetFriendRequests/GetFriendRequestsQuery.cs
+++ Application/Queries/User/FriendRequests/GetFriendRequests/GetFriendRequestsQuery.cs
@@ -3,7 +3,7 @@ using Domain.Models.Users.Enums;
 
 namespace Application.Queries.User.FriendRequests.GetFriendRequests;
 
-public class GetFriendRequestsQuery : UserRequest<GetFriendRequestsQuery.FriendRequest[]>
+public class GetFriendRequestsQuery : UserRequest<IEnumerable<GetFriendRequestsQuery.FriendRequest>>
 {
     public record FriendRequest(Guid Id, string UserName, FriendRequestStatus Status, DateTime SentAt);
 }
