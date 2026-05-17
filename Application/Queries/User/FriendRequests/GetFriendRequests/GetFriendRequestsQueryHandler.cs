25afde888f71ad76afc0283b28c1a6de00736d32
dizzydwarf1337 <stormspirit19735@gmail.com>
2026-05-17 09:15:22 +0200

    Updated get friend requests query handler

--- Application/Queries/User/FriendRequests/GetFriendRequests/GetFriendRequestsQueryHandler.cs
+++ Application/Queries/User/FriendRequests/GetFriendRequests/GetFriendRequestsQueryHandler.cs
@@ -7,21 +7,36 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Queries.User.FriendRequests.GetFriendRequests;
 
-public class GetFriendRequestsQueryHandler : IRequestHandler<GetFriendRequestsQuery, Either<Error, GetFriendRequestsQuery.Result>>
+public class
+    GetFriendRequestsQueryHandler : IRequestHandler<GetFriendRequestsQuery,
+    Either<Error, GetFriendRequestsQuery.Result>>
 {
     private readonly IUserRepository _userRepository;
-    
+
     public GetFriendRequestsQueryHandler(IUserRepository userRepository)
         => _userRepository = userRepository;
-    
-    public async Task<Either<Error, GetFriendRequestsQuery.Result>> Handle(GetFriendRequestsQuery request, CancellationToken cancellationToken)
+
+    public async Task<Either<Error, GetFriendRequestsQuery.Result>> Handle(GetFriendRequestsQuery request,
+        CancellationToken cancellationToken)
     {
-        return await _userRepository.GetByIdAsync(request.AuthorizeData!.UserId, cancellationToken)
-            .ToEitherAsync(Error.New("User not found"))
+        return await GetUser(request.AuthorizeData.UserId, cancellationToken)
             .MapAsync(u => ToResult(u.ReceivedFriendRequests, cancellationToken));
     }
 
-    public async Task<GetFriendRequestsQuery.Result> ToResult(ICollection<FriendRequest> friendRequests,
+    private async Task<Either<Error, Domain.Models.Users.User>> GetUser(Guid userId, CancellationToken token)
+    {
+        var user = await _userRepository
+            .GetAllEntities()
+            .Include(x => x.ReceivedFriendRequests)
+            .FirstOrDefaultAsync(x => x.Id == userId, token);
+
+        if (user == null)
+            return Error.New("User not found");
+
+        return user;
+    }
+
+    private async Task<GetFriendRequestsQuery.Result> ToResult(ICollection<FriendRequest> friendRequests,
         CancellationToken cancellationToken)
     {
         var senderIds = friendRequests.Select(fr => fr.SentBy).Distinct().ToList();
@@ -30,9 +45,9 @@ public class GetFriendRequestsQueryHandler : IRequestHandler<GetFriendRequestsQu
             .Where(u => senderIds.Contains(u.Id))
             .Select(u => new { u.Id, u.UserName })
             .ToListAsync(cancellationToken);
-        
+
         var senderDict = senders.ToDictionary(u => u.Id, u => u.UserName);
-        
+
         var resultArray = friendRequests.Select(fr => new GetFriendRequestsQuery.FriendRequest(
             fr.Id,
             senderDict.GetValueOrDefault(fr.SentBy, "Unknown"),
