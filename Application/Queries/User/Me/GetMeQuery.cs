d85855449d511cc6dc0ffe5c60f1060121ab76d8
Daniil Z. <zemnowf@DESKTOP-EJNGLG6.>
2026-05-30 13:21:38 +0300

    refine user id

--- Application/Queries/User/Me/GetMeQuery.cs
+++ Application/Queries/User/Me/GetMeQuery.cs
@@ -5,7 +5,7 @@ namespace Application.Queries.User.Me;
 public class GetMeQuery : UserRequest<GetMeQuery.Result>
 {
     public record Result(
-        string Id,
+        Guid Id,
         string UserName,
         string Email,
         string Bio,
